b978850a7a0e50fcce311f2eab15572c84e2d028
Ian Jiang <ianjiang.ict@gmail.com>
2021-11-15 17:44:57 +0800

    Add test for run command of state-ctrl

---
+++ test/sc_test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "sc_func.h"
+
+int main(int argc, char **argv)
+{
+	char host[] = "127.0.0.1";
+	unsigned int port = 8765;
+	unsigned long long npc;
+	int ret;
+
+	ret = sc_init(host, port);
+	if (ret < 0) {
+		return -1;
+	}
+	printf("SC Test: Init\n");
+
+	ret = sc_run(&npc); if (ret < 0) { return -1; } printf("SC Test R: npc = 0x%016llx\n", npc);
+	ret = sc_run(&npc); if (ret < 0) { return -1; } printf("SC Test R: npc = 0x%016llx\n", npc);
+	ret = sc_run(&npc); if (ret < 0) { return -1; } printf("SC Test R: npc = 0x%016llx\n", npc);
+	ret = sc_run(&npc); if (ret < 0) { return -1; } printf("SC Test R: npc = 0x%016llx\n", npc);
+	ret = sc_run(&npc); if (ret < 0) { return -1; } printf("SC Test R: npc = 0x%016llx\n", npc);
+	ret = sc_run(&npc); if (ret < 0) { return -1; } printf("SC Test R: npc = 0x%016llx\n", npc);
+	ret = sc_run(&npc); if (ret < 0) { return -1; } printf("SC Test R: npc = 0x%016llx\n", npc);
+	ret = sc_run(&npc); if (ret < 0) { return -1; } printf("SC Test R: npc = 0x%016llx\n", npc);
+	
+	return 0;
+}
