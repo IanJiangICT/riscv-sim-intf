c72e5994d8851c52eb318519c86aabaca26008ee
Ian Jiang <ianjiang.ict@gmail.com>
2021-11-17 17:43:16 +0800

    Add ouput insn for module sim_intf

--- src/sim_intf.sv
+++ src/sim_intf.sv
@@ -1,13 +1,15 @@
 import "DPI-C" function int sc_init(input string host, input int port);
-import "DPI-C" function int sc_run(output longint npc);
+import "DPI-C" function int sc_run(output longint npc, output longint insn);
 
 module sim_intf(
 	input clk,
 	input longint pc_in,
 	output longint pc_out,
+	output int insn,
 	output logic miss);
 	
 	longint npc;
+	longint ni;
 
 	initial
 	begin
@@ -20,18 +22,19 @@ module sim_intf(
 	end
 
 	always_ff @(posedge clk) begin
-		check_pc(pc_in, pc_out, miss);
+		check_pc(pc_in, pc_out, insn, miss);
 	end
 
 	function void check_pc;
 		input longint pc_i;
 		output longint pc_o;
+		output int i;
 		output logic m;
 
 		int ret;
 
 		if (pc_i == npc) begin
-			ret = sc_run(npc);
+			ret = sc_run(npc, ni);
 			if (ret < 0) begin
 				$display("Error: Failed sc_run");
 				$finish();
@@ -42,6 +45,7 @@ module sim_intf(
 			m = 1;
 		end
 		pc_o = npc;
+		i = ni;
 	endfunction;
 
 endmodule
