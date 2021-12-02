c25f388183ed17536b8bb6d3ed7842ad90986cc5
Ian Jiang <ianjiang.ict@gmail.com>
2021-12-02 11:29:00 +0800

    sc_func: Add test of forcing PC

---
+++ test/sc_test2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "sc_func.h"
+
+/* Test sc_force_pc() */
+
+int main(int argc, char **argv)
+{
+	char elf[] = "./hello";
+	unsigned int port = 8600;
+	unsigned long long npc;
+	unsigned long long pc;
+	unsigned long long insn;
+	int ret;
+	int i;
+
+	ret = sc_init_sim(elf, port);
+	if (ret < 0) {
+		return -1;
+	}
+	printf("SC Test: Init\n");
+
+	for (i = 0; i < 64; i++) {
+		ret = sc_run_next(&npc, &pc, &insn);
+		if (ret < 0) { printf("SC Test: Error. sc_run_next ret = %d\n", ret); return -1; }
+		printf("SC Test R: npc = 0x%016llx, pc = 0x%016llx, insn = 0x%016llx\n", npc, pc, insn);
+		//if (pc != 0x800001c0) continue; npc = pc + 4; // Forced to the other direction which jumps again
+		if (pc != 0x800003ec) continue; npc = pc + 4;   // Forced to the other direction which returns
+		printf("SC Test: Branch found. Force NPC to 0x%llx...\n", npc);
+		ret = sc_force_pc(npc);
+		if (ret < 0) { printf("SC Test: Error. sc_force_pc ret = %d\n", ret); return -1; }
+	}
+
+	return 0;
+}
