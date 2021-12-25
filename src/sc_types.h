005f4305d4f4374a7fd481f17ca895ab8270d2a7
Ian Jiang <ianjiang.ict@gmail.com>
2021-12-25 17:14:27 +0800

    sc_func: Define code for RISCV extension of instruction

--- src/sc_types.h
+++ src/sc_types.h
@@ -1,6 +1,36 @@
 #ifndef SC_TYPES_H
 #define SC_TYPES_H
 
+enum insn_ext {
+	RV0 = 0,
+	RVA = 'A',
+	RVB,
+	RVC,
+	RVD,
+	RVE,
+	RVF,
+	RVG,
+	RVH,
+	RVI,
+	RVJ,
+	RVK,
+	RVL,
+	RVM,
+	RVN,
+	RVO,
+	RVP,
+	RVQ,
+	RVR,
+	RVS,
+	RVT,
+	RVU,
+	RVV,
+	RVW,
+	RVX,
+	RVY,
+	RVZ
+};
+
 #define INSN_DISASM_MAX_LEN 64
 typedef struct insn_info {
 	unsigned int len;
